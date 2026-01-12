d10e60566ca01f1b4347f6f7008c26d6c26ab7eb
Ömer Apaydın <omerapaydin90@gmail.com>
2026-01-13 01:28:35 +0300

    First

--- SwiftSesOynatma/ViewController.swift
+++ SwiftSesOynatma/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func basla(_ sender: Any) {
+    }
+    
+    @IBAction func durdur(_ sender: Any) {
+    }
 }
 
