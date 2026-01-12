b2d172fb5593540dff72b3c88d0a7096f8f711ae
Ömer Apaydın <omerapaydin90@gmail.com>
2026-01-13 01:37:15 +0300

    Second

--- SwiftSesOynatma/ViewController.swift
+++ SwiftSesOynatma/ViewController.swift
@@ -6,18 +6,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var sesOynatici = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        do{
+            let dosyaYolu = Bundle.main.path(forResource: "ses", ofType: "mp3")!
+            sesOynatici = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: dosyaYolu))
+            sesOynatici.prepareToPlay()
+            
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+        
+        
     }
 
     @IBAction func basla(_ sender: Any) {
+        sesOynatici.play()
     }
     
     @IBAction func durdur(_ sender: Any) {
+            sesOynatici.stop()
     }
 }
 
